ccf70cbda33884cc923225a1c108816401550da5
David Bosschaert <david.bosschaert@gmail.com>
2014-01-02 16:04:06 +0000

    wip

---
+++ pimon-subsystem/pimon-repo/build-repo.sh
@@ -0,0 +1,13 @@
+for var in "$@"
+do
+    echo $var
+    fname="${var##*/}"
+    echo $fname
+    bsn="${fname%-*}"
+    echo $bsn
+    suffix="${fname##*-}"
+    version="${suffix%%.jar}"
+    echo $version
+
+    mvn org.apache.felix:maven-obr-plugin:1.2.0:install-file -Dfile=$var -DgroupId=org.coderthoughs.obr -DartifactId=$bsn -Dversion=$version -Dpackaging=jar -DobrRepository=./repository.xml
+done
