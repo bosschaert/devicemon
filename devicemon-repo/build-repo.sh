49993fdd3e888b71c8173f5a1cb33aaa96a79392
David Bosschaert <david.bosschaert@gmail.com>
2014-01-03 11:25:49 +0000

    wip

--- devicemon-repo/build-repo.sh
+++ devicemon-repo/build-repo.sh
@@ -1,13 +1,15 @@
-for var in "$@"
-do
-    echo $var
-    fname="${var##*/}"
-    echo $fname
-    bsn="${fname%-*}"
-    echo $bsn
-    suffix="${fname##*-}"
-    version="${suffix%%.jar}"
-    echo $version
+java -jar ./bin/org.osgi.impl.bundle.bindex.jar -r repository.xml local-repo/*.jar
 
-    mvn org.apache.felix:maven-obr-plugin:1.2.0:install-file -Dfile=$var -DgroupId=org.coderthoughs.obr -DartifactId=$bsn -Dversion=$version -Dpackaging=jar -DobrRepository=./repository.xml
-done
+# for var in "$@"
+# do
+#    echo $var
+#    fname="${var##*/}"
+#    echo $fname
+#    bsn="${fname%-*}"
+#    echo $bsn
+#    suffix="${fname##*-}"
+#    version="${suffix%%.jar}"
+#    echo $version
+#
+#    mvn org.apache.felix:maven-obr-plugin:1.2.0:install-file -Dfile=$var -DgroupId=org.coderthoughs.obr -DartifactId=$bsn -Dversion=$version -Dpackaging=jar -DobrRepository=./repository.xml
+#done
