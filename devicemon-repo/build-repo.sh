2ba4fa730d6b5b7d33f756245325750d50360ff0
David Bosschaert <david.bosschaert@gmail.com>
2014-01-14 15:25:22 +0000

    wip

--- devicemon-repo/build-repo.sh
+++ devicemon-repo/build-repo.sh
@@ -1,7 +1,7 @@
 rm index.xml
 rm repository.xml
 
-java -jar ./bin/org.osgi.impl.bundle.bindex.jar -r repository.xml local-repo/*.jar
+# java -jar ./bin/org.osgi.impl.bundle.bindex.jar -r repository.xml local-repo/*.jar
 java -jar ../devicemon-subsystem-decl/lib/org.osgi.impl.bundle.repoindex.cli.jar -r index.xml --pretty local-repo/*.jar
 
 
