a408d5c05dac5c8e00017cde2aaef6adb363a930
David Bosschaert <david.bosschaert@gmail.com>
2014-05-15 20:22:14 +0200

    Add HTTP ESA file

---
+++ devicemon-http/generate-subsystem-content-header.sh
@@ -0,0 +1,33 @@
+file=$1
+file1="$file.1"
+file2="$file.2"
+
+shift
+
+echo "Subsystem-Content: " > $file1
+
+for var in "$@"
+do
+   # echo $var
+   fname="${var##*/}"
+   bsn="${fname%-*}"
+
+   if [[ "$bsn" != *slf4j* ]] && [[ "$bsn" != *geronimo* ]] && [[ "$bsn" != *swissbox* ]] && [[ "$bsn" != *base* ]] && [[ "$bsn" != *jboss* ]] && [[ "$bsn" != *xbean* ]]
+   then
+       suffix="${fname##*-}"
+       version="${suffix%%.jar}"
+       echo "$bsn;version=$version" 
+       echo "$bsn;version=$version," >> $file1
+   else
+       echo "Excluding: $bsn"
+   fi
+done
+
+tr -d '\n' < $file1 > $file2
+
+# remove the trailing comma:
+sed '$s/,$//' < "$file2" > "$file"
+echo "" >> "$file"
+
+rm $file1 $file2
+
